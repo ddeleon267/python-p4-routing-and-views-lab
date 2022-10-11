849269904b5c1ceaf29322cb3f2d94c3fa0427f1
Dominique DeLeon <dominique.a.deleon@gmail.com>
2022-10-11 15:35:19 -0400

    done:

--- app/app.py
+++ app/app.py
@@ -1,8 +1,39 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+import ipdb
 
 app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run()
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)
+    return param
+
+@app.route('/count/<int:num>')
+def count(num):
+    nums = range(num)
+    string = ''
+    for n in nums:
+        string += f'{n}\n'
+    return string
+
+@app.route('/math/<int:num1><string:operation><int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)
+    elif operation == '-':
+        return str(num1 - num2)
+    elif operation == "div":
+        return str(num1 / num2)
+    elif operation == "*":
+        return str(num1 * num2)
+    else:
+        return str(num1 % num2)
